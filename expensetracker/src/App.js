c26b3f40be82a8adaac71536d0229896a3e2202c
VellyD <viddvd3@gmail.com>
2022-09-11 11:17:32 +0300

    Recreating the start process, since node modules couldn't load properly.

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,11 +1,10 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Expense Tracker</h1>
-        <p>Loading...</p>
+      <h1>Hi there. Welcome to your personal Expense Tracker. Let's get started!</h1>
       </header>
     </div>
   );
