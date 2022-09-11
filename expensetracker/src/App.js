19f9fd218fc62131e582ad64bb72502d4acade3c
VellyD <viddvd3@gmail.com>
2022-09-11 14:05:28 +0300

    importing an image to check the element

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import img from "./imgs/alexander-grey--8a5eJ1-mmQ-unsplash.jpg"
 
 function App() {
   return (
     <div className="App">
-      <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <div class="py-8 px-8 max-w-lg mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img
           class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
-          src="/img/erin-lindford.jpg"
+          src={img}
           alt="Woman's Face"
         />
         <div class="text-center space-y-2 sm:text-left">
